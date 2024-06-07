d3535792ed50f647c878e1201b8a83e21da61c21
Amin <aminntfnd@gmail.com>
2024-06-07 16:23:42 +0330

    monte carlo pi

--- gen_index.sh
+++ gen_index.sh
@@ -4,6 +4,20 @@
 
 rm index.html
 touch index.html
+echo "<style>        
+*{
+    font-family: sans-serif;  
+}         
+li {                           
+    font-size: x-large;        
+}                             
+</style>
+
+<h2> Programs: </h2>
+<ul>" >> index.html
+
 for dir in */; do
-    echo "<a href='./${dir}'> ${dir} </a></br>" >> index.html
+    echo "<li><a href='./${dir}'> ${dir%/} </a></li>" >> index.html
 done
+
+echo "</ul>" >> index.html
