56c8be2c06ca9b5c349d491aa8f00ac8c90e29c1
Amin <aminntfnd@gmail.com>
2025-07-31 18:06:56 +0330

    better index generation

--- gen_index.sh
+++ gen_index.sh
@@ -10,25 +10,53 @@ printf "<!DOCTYPE html>
 <style>
 html,body{
     margin: 0;
-    padding: 1rem;
+    padding: 0.75rem;
 }
 *{
     font-family: sans-serif;
 }
 li {
-    font-size: x-large;
+    font-size: larger;
+}
+a {
+    text-decoration: none;
+}
+a:hover, a:focus {
+    text-decoration: underline;
+}
+.description {
+    font-size: 0.8em;
+    margin-top: 0.3em;
+    margin-bottom: 0.5em;
+    padding-left: 1em;
 }
 </style>
 </head>
 
 <body>
-<h2> Programs: </h2>
+<h2> Graphics Lab Programs: </h2>
 <ul>\n" >> index.html
 
-for dir in */; do
-    printf "  <li><a href='./${dir}'>" >> index.html
-    printf "${dir%/}" | sed -r 's/(^|_)(\w)/\U \2/g' >> index.html
-    printf "</a></li>\n" >> index.html
+
+for path in ../src/*; do
+    description=$(head -1 "$path")
+    if [[ "$description" =~ "//\s*disabled" ]]; then
+        continue;
+    elif [[ "$description" == "// *" ]]; then
+        description="${description:3}"
+    else
+        description=""
+    fi
+
+    prog=$(basename "$path" .c)
+    # Convert snake_case to human readable title
+    title=$(echo -n "$prog" | awk -F_ '{for(i=1;i<=NF;i++)$i=toupper(substr($i,1,1)) substr($i,2); print}')
+
+    echo -n "  <li><a tabindex='0' href='./${prog}'>" >> index.html
+    echo -n "$title" >> index.html
+    echo -n "</a><p class=\"description\">" >> index.html
+    echo -n "$description" >> index.html
+    echo    "</p></li>" >> index.html
 done
 
 printf "</ul>\n</body>" >> index.html
